75a75815766644d150eeebe491f624d539af6fed
MoncefBou <moncefboughal@hotmail.fr>
2021-06-30 11:03:37 +0200

    add folder routes with admin and volunteers routes files

---
+++ backend/routes/adminRoutes.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const router = express.Router()
+
+
+router.post('/excel', (req, res) => {
+
+})
+
+router.get('/students', (req, res) => {
+
+})
+
+
+
+module.exports = { adminRoutes : router}
